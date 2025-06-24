b1a049a078894f0358221b64a929e1afedfca6e0
Alkesh Baghel <alkeshsingh009@gmail.com>
2025-06-24 15:51:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 // Simple route
 app.get('/', (req, res) => {
-
   res.send('<h1 style="color: Red;">Hello from Node.js App deployed via CI/CD!</h1>');
+});
+
+// Health check route
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
 });
 
 // Start server
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
-
